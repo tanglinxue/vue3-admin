b8bbac7a21ae0234d8e8aa7f71ea0d5a42db4fc5
tanglinxue <31237473+tanglinxue@users.noreply.github.com>
2022-03-02 15:47:30 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+import request from './utils/request'
+import storage from './utils/storage'
+import api from './api'
+import store from './store'
+console.log('环境变量=>',import.meta.env)
+
+const app = createApp(App)
+
+app.config.globalProperties.$request = request
+app.config.globalProperties.$storage = storage;
+app.config.globalProperties.$api = api;
+// axios.get(config.mockApi+'/login').then(res=>{
+//   console.log(res)
+// })
+app.use(router).use(ElementPlus).use(store).mount('#app')
---
+++ src/views/Welcome.vue
@@ -0,0 +1,50 @@
+<script>
+export default{
+  name:'Welcome',
+  props:['msg']
+}
+
+
+</script>
+
+<template>
+  <div class="welcome">
+    <h1>欢迎来到登录界面-{{msg}}</h1>
+    <router-link to="/login">去登录</router-link>
+  </div>
+</template>
+
+<style lang="scss">
+.welcome {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  background-color: #fff;
+  .content {
+    position: relative;
+    bottom: 40px;
+    .sub-title {
+      font-size: 30px;
+      line-height: 42px;
+      color: #333;
+    }
+    .title {
+      font-size: 40px;
+      line-height: 62px;
+      color: #409eff;
+    }
+    .desc {
+      text-align: right;
+      font-size: 14px;
+      color: #999;
+    }
+  }
+  .img {
+    margin-left: 105px;
+    background-image: url("./../assets/images/welcome.png");
+    width: 371px;
+    height: 438px;
+  }
+}
+</style>
